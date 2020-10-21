4dafde546d27f8700ddb7b52a7b47155f4e53dc1
Luis <lc6251495@gmail.com>
2020-10-21 14:28:49 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#Site Responsivo Simples
+
+esse é um site totalmente responsivo feito em html javascript e css
+
---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Javascript</title>
+	<meta name="viewport" content="width=device-width"ininial-scale=1.0;maximum-scale=1.0>
+	<meta name='description'content='Ola seja bem vindo'>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+<body>
+	
+<section class="main">
+	<div class="layer"></div>
+	<div class="center">
+	<header>
+		<div class="icon-social">
+		<a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+		<a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+	</div><!--icons-social-->
+	<div class="clear"></div>
+		<div class="logo">
+			FINAL
+		</div><!--Logo-->
+		<nav class="desktop">
+			<ul>
+				<li><a href="#home">Home</a></li>
+				<li><a href="#more">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul><!--Menu-->
+		</nav>
+		<nav class="mobile">
+			<i class="fa fa-bars"></i>
+			<ul>
+				<li><a href="#home">Home</a></li>
+				<li><a href="#more">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul><!--Mobile Menu-->
+		</nav>
+		<div class="clear"></div>
+	</header>
+	<div id="home" class="conteudo-header">
+		<h2>Welcome to our <span>website!</span></h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nunc vel neque malesuada ullamcorper quis tincidunt tellus. Curabitur nec auctor eros, et rhoncus lorem. Quisque sollicitudin condimentum fermentum. Sed massa erat, tempus vel blandit eu, tempus id dolor. Quisque sit amet mi eget ex interdum auctor. Donec varius, odio a condimentum finibus, odio elit posuere sem, quis mattis dui purus et ante. Pellentesque massa odio, imperdiet sit amet efficitur ut, bibendum sit amet lectus. Donec id purus semper, auctor tellus vel, rutrum tortor.</p>
+		<p>Etiam suscipit vitae dui at dictum. Suspendisse potenti. Sed ac nulla sit amet urna mollis pulvinar. Sed vitae erat ac lacus facilisis cursus vel maximus lacus. Donec tincidunt neque auctor neque scelerisque mattis. Donec iaculis turpis id viverra convallis. Mauris consequat mauris a nulla mollis, non pulvinar ligula condimentum. Sed non urna diam. Nam et consequat nisi, sit amet imperdiet mauris. Maecenas mauris est, congue non hendrerit ut, ultricies at magna. Integer porttitor, ex quis sagittis aliquet, felis nibh consectetur leo, ac suscipit augue mi eu sapien.</p>
+		<a href="#">Read More</a>
+	</div><!--Content-->
+</div><!--Center-->
+</section><!--Main-->
+<section id="more" class="planos">
+	<div class="chamada-planos">
+		<div class="center">
+			<h2>See Our Plans</h2>
+		</div>	
+	</div>
+<section class="box-planos">
+	<div class="center">
+		<div class="box-planos-single">
+			<div class="box-planos-wraper">
+			<h2>Lorem Ipsum</h2>
+			<div class="pontos-planos">
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+			</div><!--the plans-->
+			<div class="preco">
+				<h2>$120,00</h2>
+			</div>
+		</div>
+		</div>
+		<div class="box-planos-single">
+			<div class="box-planos-wraper">
+			<h2>Lorem Ipsum</h2>
+			<div class="pontos-planos">
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+			</div><!--the plans-->
+			<div class="preco">
+				<h2>$120,00</h2>
+			</div>
+		</div>
+		</div>	
+		<div class="box-planos-single">
+			<div class="box-planos-wraper">
+			<h2>Lorem Ipsum</h2>
+			<div class="pontos-planos">
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+				<p><i class="fa fa-check-square"></i>Lorem Ipsum silor donor</p>
+			</div><!--the plans-->
+			<div class="preco">
+				<h2>$120,00</h2>
+			</div>
+		</div>
+		</div>		
+	</div>
+</section>
+</section><!---Box-Plans--->
+<section class="parallax">
+	<div class="overlay-parallax"></div>
+	<div class="center">
+		<h2>More about our <span style="color:#2692FF;">Company</span></h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu nunc vel neque malesuada ullamcorper quis tincidunt tellus. Curabitur nec auctor eros, et rhoncus lorem. Quisque sollicitudin condimentum fermentum. Sed massa erat, tempus vel blandit eu, tempus id dolor. Quisque sit amet mi eget ex interdum auctor. Donec varius, odio a condimentum finibus, odio elit posuere sem, quis mattis dui purus et ante. Pellentesque massa odio, imperdiet sit amet efficitur ut, bibendum sit amet lectus. Donec id purus semper, auctor tellus vel, rutrum tortor.</p>
+		<p>Etiam suscipit vitae dui at dictum. Suspendisse potenti. Sed ac nulla sit amet urna mollis pulvinar. Sed vitae erat ac lacus facilisis cursus vel maximus lacus. Donec tincidunt neque auctor neque scelerisque mattis. Donec iaculis turpis id viverra convallis. Mauris consequat mauris a nulla mollis, non pulvinar ligula condimentum. Sed non urna diam. Nam et consequat nisi, sit amet imperdiet mauris. Maecenas mauris est, congue non hendrerit ut, ultricies at magna. Integer porttitor, ex quis sagittis aliquet, felis nibh consectetur leo, ac suscipit augue mi eu sapien.</p>
+	</div>
+</section><!---parallax--->
+<section id="contact" class="final-section">
+	<div class="center">
+		<div class="w50 contato">
+			<h2><div class="border-efeito"></div>Talk whith us!</h2>
+			<form>
+				<input type="text" placeholder="Your name..." name="">
+				<input type="email" placeholder="Your email.." name="">
+				<textarea placeholder="your message..."></textarea>
+				<input type="submit" value="Send" name="">
+			</form>
+		</div>
+		<div class="w50 time">
+			<h2><div class="border-efeito"></div>Met our team!</h2>
+			<div class="w50 equipe-single">
+				<div class="topo-box-usuario">
+				<div class="box-avatar">
+					
+				</div><!----avatar---->
+				<div class="info-usuario">
+					<p style="font-weight: bold;">Luis Geremias</p>
+					<p style="font-weight: italic;">Estudante/Programador</p>
+				</div><!----info usuario---->
+			</div><!----box usuario---->
+			<div class="descricao-usuario-box">
+				<p>Etiam suscipit vitae dui at dictum. Suspendisse potenti. Sed ac nulla sit amet urna mollis pulvinar. Sed vitae erat ac lacus facilisis cursus vel maximus lacus. Donec tincidunt neque auctor neque scelerisque mattis.</p>
+			</div>
+			</div>
+				<div class="w50 equipe-single">
+				<div class="topo-box-usuario">
+				<div class="box-avatar">
+					
+				</div><!----avatar---->
+				<div class="info-usuario">
+					<p style="font-weight: bold;">Luis Geremias</p>
+					<p style="font-weight: italic;">Estudante/Programador</p>
+				</div><!----info usuario---->
+			</div><!----box usuario---->
+			<div class="descricao-usuario-box">
+				<p>Etiam suscipit vitae dui at dictum. Suspendisse potenti. Sed ac nulla sit amet urna mollis pulvinar. Sed vitae erat ac lacus facilisis cursus vel maximus lacus. Donec tincidunt neque auctor neque scelerisque mattis.</p>
+			</div>
+			</div>
+			<div class="clear"></div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</section><!--final section--->
+<footer>
+	<p>Todos os direitos reservados a &copy; Final</p>
+</footer>
+<script src="js/jquery.js"></script>
+<script>
+	$(function(){
+
+		$('nav.mobile i').click(function(){
+			var el = $(this).parent().find('ul');
+		if (el.is(':visible')== false) {
+			el.fadeIn();
+		}else{
+			el.fadeOut();
+		}
+		})
+	})
+</script>
+</body>
+</html> 
